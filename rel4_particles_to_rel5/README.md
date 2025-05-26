151dfa6c32b591b2218e3fdb9691685a7b8001c6
Sander Roet <sanderroet@hotmail.com>
2025-05-26 11:07:16 +0200

    add convert_rel4_rel5.py (#10)
    * add convert_rel4_rel5.py
    
    * Update convert_rel4_rel5.py
    
    * Update rel4_particles_to_rel5/convert_rel4_rel5.py
    
    Co-authored-by: Marten Chaillet <58044494+McHaillet@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Marten Chaillet <58044494+McHaillet@users.noreply.github.com>

---
+++ rel4_particles_to_rel5/README.md
@@ -0,0 +1,3 @@
+# Convert RELION4 particle .star files to RELION5
+this script will convert a RELION4 style particle.star file to a RELION5 style. 
+You give it an input .star and the dimensions of the tomogram in pixels for x, y, and z
---
+++ rel4_particles_to_rel5/convert_rel4_rel5.py
@@ -0,0 +1,52 @@
+import starfile
+import pandas as pd
+import argparse
+
+def scale_coordinates(star_filename, pixel_sizes):
+    # Load the STAR file
+    data = starfile.read(star_filename)
+    # Check if the required columns exist
+    required_columns = ['rlnCoordinateX', 'rlnCoordinateY', 'rlnCoordinateZ', 'rlnDetectorPixelSize']
+    if not all(col in data.columns for col in required_columns):
+        print(data.columns)
+        print(f"Missing columns: {set(required_columns)-set(data.columns)}")
+        raise ValueError(f"A RELION4 STAR file must contain the columns: {required_columns}")
+
+    # Check if the required columns exist
+    # same guestimate pytom-match-pick uses
+    center_x = pixel_sizes[0] / 2
+    center_y = pixel_sizes[1] / 2
+    center_z = pixel_sizes[2] / 2
+    # Multiply each coordinate by the corresponding pixel size and detector pixel size
+    data['rlnCoordinateX'] = (data['rlnCoordinateX'] - center_x) * data['rlnDetectorPixelSize']
+    data['rlnCoordinateY'] = (data['rlnCoordinateY'] - center_y) * data['rlnDetectorPixelSize']
+    data['rlnCoordinateZ'] = (data['rlnCoordinateZ'] - center_z) * data['rlnDetectorPixelSize']
+
+    column_change = {
+        "rlnCoordinateX": "rlnCenteredCoordinateXAngst",
+        "rlnCoordinateY": "rlnCenteredCoordinateYAngst",
+        "rlnCoordinateZ": "rlnCenteredCoordinateZAngst",
+        "rlnMicrographName": "rlnTomoName",
+        "rlnDetectorPixelSize": "rlnTomoTiltSeriesPixelSize",
+    }
+    data = data.rename(columns=column_change)
+
+    # Write the updated data to a new STAR file
+    output_filename = star_filename.replace('.star', '_rf5.star')
+    starfile.write({"particles":data}, output_filename, overwrite=True)
+
+    print(f"RELION5 coordinates written to: {output_filename}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Convert a RELION4 particle .star file to a RELION5 star file. This requires input on the dimensions of the tomogram used for picking."
+    )
+    parser.add_argument("starfile", type=str, help="Path to the input STAR file")
+    parser.add_argument("pixel_x", type=float, help="Number of pixels in X dimension")
+    parser.add_argument("pixel_y", type=float, help="Number of pixels in Y dimension")
+    parser.add_argument("pixel_z", type=float, help="Number of pixels in Z dimension")
+
+    args = parser.parse_args()
+    pixel_sizes = (args.pixel_x, args.pixel_y, args.pixel_z)
+
+    scale_coordinates(args.starfile, pixel_sizes)
